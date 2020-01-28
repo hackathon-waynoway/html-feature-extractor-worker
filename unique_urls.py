220f6c6566bfb1f857eeb5df3a09114b208acd51
Brian McCarthy <bmccarthy@billtrust.com>
2020-01-28 13:09:58 -0500

    more updates

---
+++ unique_urls.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import sys
+import gzip
+
+urls = {}
+destFileNmae = sys.argv[1] # output filename
+
+count = 0
+with gzip.open('data/schema_Event.gz','rt') as f:
+    for line in f:
+        # If line is empty then end of file reached
+        if not line :
+            break
+        parsedLine = line.split()
+
+        url = parsedLine[-2].replace('<', '').replace('>', '')
+        
+        if url not in urls:
+            urls[url] = 1 
+            count = count + 1
+        
+        # if count == 10: break
+
+with open(destFileNmae, "w") as f:
+    for url in urls:
+        print(url, file=f)
