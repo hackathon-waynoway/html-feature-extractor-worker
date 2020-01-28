a16af490fe87eaa9fc36153127a859e1a81dd052
Brian McCarthy <bmccarthy@billtrust.com>
2020-01-28 09:12:46 -0500

    make s3 objects public

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,5 +1,6 @@
 resource "aws_s3_bucket" "output" {
   bucket = "${var.application}-output"
+  acl = "public-read"
 
   lifecycle_rule {
     id      = "expiration"
@@ -9,4 +10,21 @@ resource "aws_s3_bucket" "output" {
       days = 30
     }
   }
+
+  policy = <<EOF
+{
+    "Version": "2008-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadForGetBucketObjects",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "*"
+            },
+            "Action": "s3:GetObject",
+            "Resource": "arn:aws:s3:::${var.application}-output/*"
+        }
+    ]
+}
+EOF
 }
