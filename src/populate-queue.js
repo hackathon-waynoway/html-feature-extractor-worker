1395f01df8b5945c3d7bee48793fdc468b6acea0
Brian McCarthy <bmccarthy@billtrust.com>
2020-01-28 09:14:16 -0500

    cleanup

--- src/populate-queue.js
+++ src/populate-queue.js
@@ -1,8 +1,13 @@
 
 const AWS = require('aws-sdk');
-const shortid = require('shortid');
+const program = require('commander');
 
-const source = shortid.generate();
+const options = program
+    .option('-v, --verbose', 'verbose', false)
+    .option('-s, --src <value>', 'File with urls to crawl (one per line)')
+    .option('-c, --count <value>', 'number of urls to crawl', 100)
+    .parse(process.argv).opts();
+
 const sqs = new AWS.SQS();
 
 async function main() {
@@ -11,6 +16,9 @@ async function main() {
     // TODO: don't want to spam sites, so maybe only take unique domains?
     // TODO: Might want to filter only webpages which are in the USA 
     // TODO: limit this list to ~100 to start and send those SQS messages
+
+    const date = new Date();
+    const source = `${date.getFullYear()}_${date.getMonth() + 1}_${date.getDate()}`;
 
     const queueUrl = `https://sqs.us-east-1.amazonaws.com/109628666462/html-feature-extractor-worker-queue`
     const message = { url: 'https://losangeles.eventful.com/events/categories/music', source: source };
